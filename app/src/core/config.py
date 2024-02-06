d4eb70ea9e6c49299ac7a72a43d7f34deae16921
egor nikitin <eanikitin90gmail.com>
2024-02-06 12:05:41 +0400

    base sceleton + congifs

---
+++ app/src/core/config.py
@@ -0,0 +1,34 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RedisSettings(BaseSettings):
+
+    host: str
+    port: int
+    db: int
+
+
+class PostgresSettings(BaseSettings):
+
+    password: str
+    user: str
+    db: str
+    host: str
+    port: int
+    schema: str
+
+class Settings(BaseSettings):
+
+    model_config = SettingsConfigDict(
+        env_file='../../.env',
+        env_file_encoding='utf-8',
+        extra='ignore',
+        env_nested_delimiter='__'
+    )
+
+    redis: RedisSettings
+    postgres: PostgresSettings
+    echo: bool = True
+    log_level: str
+
+settings = Settings()
---
+++ app/src/db/sqlalcem.py
@@ -0,0 +1,20 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+from core.config import settings
+
+
+Base = declarative_base(metadata=MetaData(schema="memo"))
+
+dsn = f'''postgresql+asyncpg://{
+    settings.postgres.user}:{settings.postgres.password}@{
+        settings.postgres.host}:{settings.postgres.port}/{settings.postgres.db}'''
+engine = create_async_engine(dsn, echo=settings.echo, future=True)
+async_session = sessionmaker(engine, class_=AsyncSession,
+                             expire_on_commit=False)
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
